457b9a0f6f4c824e2394edf91f267f195446b962
owengaming001 <42657486+owengaming001@users.noreply.github.com>
2021-04-05 11:11:47 -0230

    First Injection 1.0.1 (3/3)
    Patch Notes:
    -Added better controller support for Eden because he was complaining about the controls.
    -Changed the timing on both QuW and Injection Cube's roundhouse attacks. (Sorry, I forgot to mention that one before) They're now a lot faster. But QuW is still best girl.

--- Controllers/EdenJoystick.py
+++ Controllers/EdenJoystick.py
@@ -2,20 +2,49 @@ class Controller:
 	def __init__(self,pygame):
 		self.Joystick=pygame.joystick.Joystick(0)
 		self.Joystick.init()
+		self.current = False
 		pass
+
 	def Character(self,pygame):
+		
+		Y=0
+		B=1
+		A=2
+		X=3
+		L=4
+		R=5
+		ZL=6
+		ZR=7
+		Minus=8
+		Plus=9
+		JSL=10
+		JSR=11
+		Home=12
+		Capture=13
+		JS1RL=0
+		JS1DU=1
+		JS2RL=2
+		JS2DU=3
+		#hat(0)[0] is RL, hat(0)[1] is DU 
+		"""last=self.current
+		self.current=self.Joystick.get_button(Y)
+		if (self.current != last) and (self.current == True):
+			print("")
+			print(self.Joystick.get_hat(0)[0])
+			print(self.Joystick.get_hat(0)[1])"""
+
 		return {
-		"X":max(-1,int(self.Joystick.get_axis(0)*2)),
-		"Y":max(-1,int(self.Joystick.get_axis(1)*2)),
+		"X":max(-1,int(self.Joystick.get_axis(0)*1.99)),
+		"Y":max(-1,int(self.Joystick.get_axis(1)*1.99)),
 		"Jump":self.Joystick.get_axis(1)<-0.5,
-		"Jab":self.Joystick.get_button(0),
-		"Strong":self.Joystick.get_button(1),
-		"Fierce":self.Joystick.get_button(2),
-		"Short":self.Joystick.get_button(3),
-		"Forward":self.Joystick.get_button(4),
-		"Roundhouse":self.Joystick.get_button(5),
-		"Block":self.Joystick.get_axis(2)<-0.5,
-		"Special":self.Joystick.get_axis(2)>0.5,
+		"Jab":self.Joystick.get_button(Y),
+		"Strong":self.Joystick.get_button(X),
+		"Fierce":self.Joystick.get_button(L),
+		"Short":self.Joystick.get_button(B),
+		"Forward":self.Joystick.get_button(A),
+		"Roundhouse":self.Joystick.get_button(ZR),
+		"Block":self.Joystick.get_button(ZL),
+		"Special":self.Joystick.get_button(R),
 		}
 		pass
 	pass
