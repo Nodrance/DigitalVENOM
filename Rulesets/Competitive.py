9b3f5736706bdfafad9f16b48460ee387a117907
barry sheppard <barry sheppard@barry>
2021-08-26 23:56:59 -0250

    Second Injection 2.0.0

--- Rulesets/Competitive.py
+++ Rulesets/Competitive.py
@@ -1,18 +1,12 @@
 def Match(pygame,Renderer,Engine,P1C,P2C,F1,F2,BG):
 	GameStartSound=pygame.mixer.Sound("Sounds/GameStart0.wav")
 	GameOn=1
-	P1=F1(0,pygame)
-	P2=F2(1,pygame)
+	P1=F1#(0,pygame)
+	P2=F2#(1,pygame)
 	while GameOn:
-		try:
-			P1.Reset(0,pygame)
-		except:
-			P1.__init__(0,pygame)
-		try:
-			P2.Reset(1,pygame)
-		except:
-			P2.__init__(1,pygame)
-		Y,X=Engine.Game(P1,P2,Renderer,pygame,P1C,P2C,BG,GameStartSound)
+		P1.Reset(0,pygame)
+		P2.Reset(1,pygame)
+		Y,X=Engine.Game(P1,P2,Renderer,pygame,P1C,P2C,BG,GameStartSound,SaveReplay=1)
 		if X and not Y:
 			if Renderer.P1W:
 				GameOn=False
@@ -26,5 +20,7 @@ def Match(pygame,Renderer,Engine,P1C,P2C,F1,F2,BG):
 			else:
 				Renderer.P2W+=1
 			pass
+		if not (X or Y):
+			return (0,0)
 		pass
 	pass
--- Stages/VenomWarmup.py
+++ Stages/VenomWarmup.py
@@ -1,24 +1,33 @@
 class Stage:
 	def __init__(self,pygame):
-		self.Bounds=[-750,750,-1000]
+		self.Fault=[
+		F0(pygame),
+		]
+		self.DuelFaultCameraOffset=200
+		self.FaultOffset=0
+		self.Bounds=[-750,750,-750]
 		self.Sprites=[{
-		"Sprite":pygame.image.load("Stages/VenomWarmup/Background.png"),
-		"X":0,
-		"Y":0,
-		"Z":3,
-		"H":5000,
-		"W":5000,
-		"Large":1,
-		"Blending":None,
-		},
-		{
-		"Sprite":pygame.image.load("Stages/VenomWarmup/Floor.png"),
+		"Sprite":pygame.image.load("Stages/VenomWarmup/BG.bmp"),
 		"X":0,
 		"Y":0,
 		"Z":0,
-		"H":2500,
-		"W":2500,
-		"Large":1,
+		"H":1500,
+		"W":1500,
+		"Large":2,
+		"Blending":None,
+		}]
+		pass
+class F0:
+	def __init__(self,pygame):
+		self.Bounds=[-750,750,-750]
+		self.Sprites=[{
+		"Sprite":pygame.image.load("Stages/VenomWarmup/BG.bmp"),
+		"X":0,
+		"Y":0,
+		"Z":0,
+		"H":3000,
+		"W":3000,
+		"Large":2,
 		"Blending":None,
 		}]
 		pass
